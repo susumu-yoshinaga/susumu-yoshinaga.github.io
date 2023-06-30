6b58598c3b402881effe14410510f149f7a13957
susumu-yoshinaga <132860236+susumu-yoshinaga@users.noreply.github.com>
2023-06-30 18:56:50 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,180 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
 
+
+document.querySelector("#kakunou").addEventListener(
+    "click",
+    function () {
+        document.getElementById("susumu1").style.visibility = "visible";
+        document.getElementById("susumu2").style.visibility = "hidden";
+        document.getElementById("susumu3").style.visibility = "hidden";
+        document.getElementById("susumu4").style.visibility = "hidden";
+        document.getElementById("susumu5").style.visibility = "hidden";
+        document.getElementById("susumu6").style.visibility = "hidden";
+        document.getElementById("susumu7").style.visibility = "hidden";
+        document.getElementById("susumu8").style.visibility = "hidden";
+        document.getElementById("susumu9").style.visibility = "hidden";
+    //   console.log ("Thank you");
+    },
+    false
+  );
+
+ document.querySelector("#kakunouA").addEventListener(
+    "click",
+    function () {
+        // console.log ("Thank you");
+        document.getElementById("susumu1").style.visibility = "hidden";
+        document.getElementById("susumu2").style.visibility = "visible";
+        document.getElementById("susumu3").style.visibility = "hidden";
+        document.getElementById("susumu4").style.visibility = "hidden";
+        document.getElementById("susumu5").style.visibility = "hidden";
+        document.getElementById("susumu6").style.visibility = "hidden";
+        document.getElementById("susumu7").style.visibility = "hidden";
+        document.getElementById("susumu8").style.visibility = "hidden";
+        document.getElementById("susumu9").style.visibility = "hidden";
+
+    },
+    false
+  );
+
+  document.querySelector("#kakunouE").addEventListener(
+    "click",
+    function () {
+        // console.log ("Thank you");
+        document.getElementById("susumu1").style.visibility = "hidden";
+        document.getElementById("susumu2").style.visibility = "hidden";
+        document.getElementById("susumu3").style.visibility = "visible";
+        document.getElementById("susumu4").style.visibility = "hidden";
+        document.getElementById("susumu5").style.visibility = "hidden";
+        document.getElementById("susumu6").style.visibility = "hidden";
+        document.getElementById("susumu7").style.visibility = "hidden";
+        document.getElementById("susumu8").style.visibility = "hidden";
+        document.getElementById("susumu9").style.visibility = "hidden";
+
+    },
+    false
+  );
+  document.querySelector("#kakunouG").addEventListener(
+    "click",
+    function () {
+        // console.log ("Thank you");
+        document.getElementById("susumu1").style.visibility = "hidden";
+        document.getElementById("susumu2").style.visibility = "hidden";
+        document.getElementById("susumu3").style.visibility = "hidden";
+        document.getElementById("susumu4").style.visibility = "visible";
+        document.getElementById("susumu5").style.visibility = "hidden";
+        document.getElementById("susumu6").style.visibility = "hidden";
+        document.getElementById("susumu7").style.visibility = "hidden";
+        document.getElementById("susumu8").style.visibility = "hidden";
+        document.getElementById("susumu9").style.visibility = "hidden";
+
+    },
+    false
+  );
+
+  document.querySelector("#kakunouI").addEventListener(
+    "click",
+    function () {
+        // console.log ("Thank you");
+        document.getElementById("susumu1").style.visibility = "hidden";
+        document.getElementById("susumu2").style.visibility = "hidden";
+        document.getElementById("susumu3").style.visibility = "hidden";
+        document.getElementById("susumu4").style.visibility = "hidden";
+        document.getElementById("susumu5").style.visibility = "visible";
+        document.getElementById("susumu6").style.visibility = "hidden";
+        document.getElementById("susumu7").style.visibility = "hidden";
+        document.getElementById("susumu8").style.visibility = "hidden";
+        document.getElementById("susumu9").style.visibility = "hidden";
+
+    },
+    false
+  );
+
+  document.querySelector("#kakunouK").addEventListener(
+    "click",
+    function () {
+        // console.log ("Thank you");
+        document.getElementById("susumu1").style.visibility = "hidden";
+        document.getElementById("susumu2").style.visibility = "hidden";
+        document.getElementById("susumu3").style.visibility = "hidden";
+        document.getElementById("susumu4").style.visibility = "hidden";
+        document.getElementById("susumu5").style.visibility = "hidden";
+        document.getElementById("susumu6").style.visibility = "visible";
+        document.getElementById("susumu7").style.visibility = "hidden";
+        document.getElementById("susumu8").style.visibility = "hidden";
+        document.getElementById("susumu9").style.visibility = "hidden";
+
+    },
+    false
+  );
+  document.querySelector("#kakunouM").addEventListener(
+    "click",
+    function () {
+        // console.log ("Thank you");
+        document.getElementById("susumu1").style.visibility = "hidden";
+        document.getElementById("susumu2").style.visibility = "hidden";
+        document.getElementById("susumu3").style.visibility = "hidden";
+        document.getElementById("susumu4").style.visibility = "hidden";
+        document.getElementById("susumu5").style.visibility = "hidden";
+        document.getElementById("susumu6").style.visibility = "hidden";
+        document.getElementById("susumu7").style.visibility = "visible";
+        document.getElementById("susumu8").style.visibility = "hidden";
+        document.getElementById("susumu9").style.visibility = "hidden";
+
+    },
+    false
+  );
+
+  document.querySelector("#kakunouO").addEventListener(
+    "click",
+    function () {
+        // console.log ("Thank you");
+        document.getElementById("susumu1").style.visibility = "hidden";
+        document.getElementById("susumu2").style.visibility = "hidden";
+        document.getElementById("susumu3").style.visibility = "hidden";
+        document.getElementById("susumu4").style.visibility = "hidden";
+        document.getElementById("susumu5").style.visibility = "hidden";
+        document.getElementById("susumu6").style.visibility = "hidden";
+        document.getElementById("susumu7").style.visibility = "hidden";
+        document.getElementById("susumu8").style.visibility = "visible";
+        document.getElementById("susumu9").style.visibility = "hidden";
+
+    },
+    false
+  );
+
+  document.querySelector("#kakunouP").addEventListener(
+    "click",
+    function () {
+        // console.log ("Thank you");
+        document.getElementById("susumu1").style.visibility = "hidden";
+        document.getElementById("susumu2").style.visibility = "hidden";
+        document.getElementById("susumu3").style.visibility = "hidden";
+        document.getElementById("susumu4").style.visibility = "hidden";
+        document.getElementById("susumu5").style.visibility = "hidden";
+        document.getElementById("susumu6").style.visibility = "hidden";
+        document.getElementById("susumu7").style.visibility = "hidden";
+        document.getElementById("susumu8").style.visibility = "hidden";
+        document.getElementById("susumu9").style.visibility = "visible";
+
+    },
+    false
+  );
+
+// function clickSelect(){
+//     console.log("ok")
+// }
+
+    // "click",
+//     function () {
+//       alert("Thank you");
+//     },
+//     false
+//   );
+
+
+// function imageChange (){
+
+
+
+// }
